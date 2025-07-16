c03bdd69b069fcbe07b6c721ed814f06342bc692
hyp <james.h.e@foxmail.com>
2025-07-17 00:15:02 +0800

    feature: 开发短链接后管分组新增功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/dao/mapper/GroupMapper.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dao/mapper/GroupMapper.java
@@ -2,11 +2,10 @@ package com.nageoffer.shortlink.admin.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
-import org.apache.shardingsphere.infra.rule.identifier.scope.GlobalRule;
 
 /**
  * 短链接分组持久层
  */
-public class GroupMapper extends BaseMapper<GroupDO> {
+public interface GroupMapper extends BaseMapper<GroupDO> {
 
 }
